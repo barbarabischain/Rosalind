22aa02ae05dfbb1aaafd62f59bc4b8ac8743c660
cambacica <bbischain@gmail.com>
2024-09-13 16:43:12 -0300

    first commit

---
+++ CountDNA/countdna.py
@@ -0,0 +1,18 @@
+def counter():
+    dna = open("rosalind_dna.txt", "r")
+    sequence = dna.read()
+    countA = 0
+    countT = 0
+    countC = 0
+    countG = 0
+    for base in sequence:
+        if (base == "A"):
+            countA += 1
+        elif (base == "T"):
+            countT += 1
+        elif (base == "C"):
+            countC += 1
+        elif (base == "G"):
+            countG += 1
+    return(countA, countC, countG, countT)
+counter()
